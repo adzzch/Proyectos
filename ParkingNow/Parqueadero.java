e1ff0162b9c4a4c84071ef3ab6a833a1a61b7d1e
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-03-11 22:02:42 +0000

    g

--- ParkingNow/Parqueadero.java
+++ ParkingNow/Parqueadero.java
@@ -5,8 +5,8 @@ public class Parqueadero {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         //Arrreglos para controlar la disponibilidad de puestos
-        boolean[] bajoCC = new boolean[20];  //20 puestos para las motos de baja cc(Falso es = libre)
-        boolean[] altoCC = new boolean[10]; //10 Puestos para las motos de alto cc
+        boolean[] bajosCC = new boolean[20];  //20 puestos para las motos de baja cc(Falso es = libre)
+        boolean[] altosCC = new boolean[10]; //10 Puestos para las motos de alto cc
         String[] horasEntrada = new String[30]; //Almacena la hora en la que entra (índicees 0-19_ bajo)
         
         while (true) { // Menu interactivo
