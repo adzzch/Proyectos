d1100087e5d475ce845f38e9c8bc654572a8d555
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-03-11 20:47:12 +0000

    Guardar registro (Entrada de la moto)

--- ParkingNow/Parqueadero.java
+++ ParkingNow/Parqueadero.java
@@ -6,8 +6,8 @@ public class Parqueadero {
         Scanner scanner = new Scanner(System.in);
         //Arrreglos para controlar la disponibilidad de puestos
         boolean[] bajoCC = new boolean[20];  //20 puestos para las motos de baja cc(Falso es = libre)
-        boolean[] altosCC = new boolean[10]; //10 Puestos para las motos de alto cc
-        String[] horasEntrada = new String[30]; //Almacena la hora en la que entra (índicees 0-19_ bajo)
+        boolean[] altoCC = new boolean[10]; //10 Puestos para las motos de alto cc
+        String[] horaEntrada = new String[30]; //Almacena la hora en la que entra (índicees 0-19_ bajo)
         
         while (true) { // Menu interactivo
             System.out.println("--- MENÚ ---");
@@ -32,10 +32,33 @@ public class Parqueadero {
                     }
                     } else {
                         System.out.println("Puestos Altos CC [D=Libre, O=Ocupado]");
-                        for(int 1 = 0; i <10; i++) {
+                        for(int i = 0; i <10; i++) {
                             System.out.print((i+1) + (altosCC[i] ? "O " : "D "));
                         }
                     }
+
+                    System.out.println("Elige el puesto que desees");
+                    int puesto = scanner.nextInt();
+
+                    // Generar una hora aleatoria entre las 07:00 y 22:00
+                    int horaEntradaMin = 420 + (int)(Math.random() * 901);
+                    String horaEntrada = String.format("%02:%02d",
+                        horaEntradaMin / 60,
+                        horaEntradaMin % 60 );
+                    System.out.println("Hora entrada generada: " + horaEntrada);
+
+                    // Guardar registro
+                    boolean exito = false;
+                    if (tipo == 1 && puesto <= 20 && !bajosCC[puesto - 1]) {
+                        bajoCC[puesto-1] = true;
+                        horasEntrada[puesto-1] = horaEntrada;
+                        exito = true;
+                    } else if (tipo == 2 && puesto <= 10 && !altosCC[puesto - 1]) {
+                        altosCC[puesto - 1] = true;
+                        horasEntrada[20 + (puesto-1)] = horaEntrada;
+                        exito = true;
+                    }
+
                 }
             }
         }
